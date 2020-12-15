cd914878547dc0fd449b7a6b8f0b70d41a9c5417
Shoncito92 <cromaticsoul@gmail.com>
2020-12-15 20:52:12 -0300

    Agregando .py principal, con instrucciones

---
+++ evaluacion_m2.py
@@ -0,0 +1,34 @@
+#Haga un programa que muestre una cantidad ingresada por el usuario en forma de abaco en pantalla:
+
+#Utilice un diccionario que almacene las unidades, decenas, centenas,
+#unidades de mil, unidades de diez mil y unidades de cien mil.
+
+#cada intento debe ser almacenado en una lista,
+#tal lista debe contener todos los intentos de usuario.
+
+#Si el usuario desea dejar de ejecutar el programa en cualquier instante,
+#permitale terminar escribiendo ‘salir’.
+
+#Previo a ‘salir’ muestre en pantalla todos los intentos de la lista, por
+
+#ejemplo:
+#841.231
+# 10.231
+#    123
+#903.267
+#   etc.
+
+#No olvide ubicar el punto separador de miles para mostrar la cifra más
+#claramente.
+
+#Haga su código legible y facilmente ejecutable, dando las instrucciones de
+#su uso.
+
+#Separe las distintas partes en funciones propiamente nombradas y de una
+#extension razonable.
+
+#Escriba todos los comentarios que estime convenientes.
+
+#No solo se evaluará que el programa haga lo requerido, también se
+#evaluará el orden del código. Códigos poco legibles no se evaluarán
+#positivamente.
